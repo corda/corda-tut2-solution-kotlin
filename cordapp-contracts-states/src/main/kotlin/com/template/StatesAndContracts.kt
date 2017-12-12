52f2e11a3041a3dff3bc12e6cffc00f4c3aee0d5
joeldudleyr3 <joel.dudley@r3.com>
2017-12-12 14:01:16 +0000

    Follows the Hello, World tutorial.

--- cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
+++ cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -3,7 +3,7 @@ package com.template
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
 import net.corda.core.contracts.ContractState
-import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
 import net.corda.core.transactions.LedgerTransaction
 
 // *****************
@@ -28,6 +28,8 @@ open class TemplateContract : Contract {
 // *********
 // * State *
 // *********
-data class TemplateState(val data: String) : ContractState {
-    override val participants: List<AbstractParty> get() = listOf()
+class IOUState(val value: Int,
+               val lender: Party,
+               val borrower: Party) : ContractState {
+    override val participants get() = listOf(lender, borrower)
 }
--- cordapp/src/main/kotlin/com/template/App.kt
+++ cordapp/src/main/kotlin/com/template/App.kt
@@ -1,14 +1,16 @@
 package com.template
 
 import co.paralleluniverse.fibers.Suspendable
-import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.Contract
-import net.corda.core.contracts.ContractState
-import net.corda.core.flows.*
-import net.corda.core.identity.AbstractParty
+import net.corda.core.contracts.Command
+import net.corda.core.flows.FinalityFlow
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.identity.Party
 import net.corda.core.messaging.CordaRPCOps
 import net.corda.core.serialization.SerializationWhitelist
-import net.corda.core.transactions.LedgerTransaction
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
 import net.corda.webserver.services.WebServerPluginRegistry
 import java.util.function.Function
 import javax.ws.rs.GET
@@ -36,18 +38,32 @@ class TemplateApi(val rpcOps: CordaRPCOps) {
 // *********
 @InitiatingFlow
 @StartableByRPC
-class Initiator : FlowLogic<Unit>() {
-    @Suspendable
-    override fun call() {
-        return Unit
-    }
-}
+class IOUFlow(val iouValue: Int,
+              val otherParty: Party) : FlowLogic<Unit>() {
 
-@InitiatedBy(Initiator::class)
-class Responder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
+    /** The progress tracker provides checkpoints indicating the progress of the flow to observers. */
+    override val progressTracker = ProgressTracker()
+
+    /** The flow logic is encapsulated within the call() method. */
     @Suspendable
     override fun call() {
-        return Unit
+        // We retrieve the notary identity from the network map.
+        val notary = serviceHub.networkMapCache.notaryIdentities[0]
+
+        // We create the transaction components.
+        val outputState = IOUState(iouValue, ourIdentity, otherParty)
+        val cmd = Command(TemplateContract.Commands.Action(), ourIdentity.owningKey)
+
+        // We create a transaction builder and add the components.
+        val txBuilder = TransactionBuilder(notary = notary)
+                .addOutputState(outputState, TEMPLATE_CONTRACT_ID)
+                .addCommand(cmd)
+
+        // We sign the transaction.
+        val signedTx = serviceHub.signInitialTransaction(txBuilder)
+
+        // We finalise the transaction.
+        subFlow(FinalityFlow(signedTx))
     }
 }
 
